de51f81e938d0a04a073582c69f13eaae10dc7cb
0x1306a94 <0x1306a94@gmail.com>
2026-09-24 18:03:34 +0800

    Migrate example apps to the UIScene lifecycle. (#690)
    Move Maps, Samples, SamplesObjC, and Stocks example apps from the AppDelegate window setup to the UIScene lifecycle. Add SceneDelegate plus UIApplicationSceneManifest so the examples launch correctly on current Xcode / iOS.

---
+++ Examples/SamplesObjC/SamplesObjC/SceneDelegate.h
@@ -0,0 +1,10 @@
+// Copyright 2026 the FloatingPanel authors. All rights reserved. MIT license.
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
